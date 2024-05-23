927e3479f6dca7a7e00c13b6362c94c958b55c28
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-05-23 16:02:41 +0530

    server issue

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom'
+import Navbar from "../components/Navbar"
 
 const Home = () => {
     const [gitLink,setGitLink] = useState('')
@@ -31,44 +32,47 @@ const Home = () => {
     }
 
     return(
-        <div className="w-screen h-screen flex justify-center items-center flex-col gap-2">
-        <div className="flex flex-col w-1/2 max-w-[30rem] gap-2">
-            <input 
-                type="text" 
-                value={gitLink}
-                onChange={(e) => setGitLink(e.target.value)}
-                disabled={isLoading}
-                placeholder="Git Link"
-                className="h-10 bg-gray-300 px-2 outline-none rounded"
-            />
+        <div className="bg-black">
+            <Navbar />
+            <div className="w-screen h-screen flex justify-center items-center flex-col gap-2">
+            <div className="flex flex-col w-1/2 max-w-[30rem] gap-2">
+                <input 
+                    type="text" 
+                    value={gitLink}
+                    onChange={(e) => setGitLink(e.target.value)}
+                    disabled={isLoading}
+                    placeholder="Github Link"
+                    className="h-10 bg-gray-300 px-2 outline-none rounded"
+                />
 
-            <input 
-                type="text" 
-                value={rootDir}
-                onChange={(e) => setRootDir(e.target.value)}
-                disabled={isLoading}
-                placeholder="Root Directory"
-                className="h-10 bg-gray-300 px-2 outline-none rounded"
-            />
+                <input 
+                    type="text" 
+                    value={rootDir}
+                    onChange={(e) => setRootDir(e.target.value)}
+                    disabled={isLoading}
+                    placeholder="Root Directory"
+                    className="h-10 bg-gray-300 px-2 outline-none rounded"
+                />
 
-            <select
-                value={stack}
-                onChange={(e) => setStack(e.target.value)}
+                <select
+                    value={stack}
+                    onChange={(e) => setStack(e.target.value)}
+                    disabled={isLoading}
+                    className="h-10 bg-gray-300 px-2 outline-none rounded"
+                >
+                    <option value="React">React</option>
+                    <option value="nextjs">Next.js</option>
+                    <option value="django">Django</option>
+                </select>
+            </div>
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
                 disabled={isLoading}
-                className="h-10 bg-gray-300 px-2 outline-none rounded"
+                onClick={Clone}
             >
-                <option value="React">React</option>
-                <option value="nextjs">Next.js</option>
-                <option value="django">Django</option>
-            </select>
-        </div>
-        <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
-            disabled={isLoading}
-            onClick={Clone}
-        >
-            {isLoading ? 'Cloning...' : 'Clone'}
-        </button>
+                {isLoading ? 'Cloning...' : 'Clone'}
+            </button>
+            </div>
         </div>
     );
 }
